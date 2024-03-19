0e98fbde5d89082f8b85445809d26ce3fc6e1e90
KatePoliakh <epolyakh.p@gmail.com>
2024-03-19 16:24:54 +0300

    feat: added GameResources logic and gameField object

---
+++ Assets/_Source/Core/GameResourse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Source.Core {
+    public enum GameResource
+    {
+        Humans,
+        Food,
+        Wood,
+        Stone,
+        Gold
+    }
+}
---
+++ Assets/_Source/Core/ObservableInt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _Source.Core
+{
+    public class ObservableInt
+    {
+        private int _value;
+        public event Action<int> ValueChanged;
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    ValueChanged?.Invoke(_value);
+                }
+            }
+        }
+    }
+}
